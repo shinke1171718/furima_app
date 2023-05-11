a4c5686a6394574f053eeae5f071aef104359efb
Apple <apple@ShinkenoMacBook-3.local>
2023-05-11 11:17:43 +0900

    “ログイン時にitemのindex.html.erbになるよう設定”

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   get 'item/create'
   get 'item/update'
   get 'item/delete'
+
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  root to: 'item#index'
 end
